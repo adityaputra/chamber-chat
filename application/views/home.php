7fb7ba408474839ee80c4398d1a6a1cf6b186243
Aditya <adityaf.putra@gmail.com>
2013-07-16 13:58:18 +0700

    first commit, put codeigniter framework into repo

---
+++ application/views/home.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>
+			Log into chat - AdityaPutra.com
+		</title>
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			Please enter your name and passkey to login
+	        <div id="menu" align="center">
+	        	<table>
+	        	<form>
+	        		<tr><td>Nickname</td><td>:</td><td><input type="text" name="uname" id="uname"/></td></tr>
+	        		<tr><td>Passkey</td><td>:</td><td><input type="password" name="passkey" id="passkey"/></td></tr>
+	        		<tr><td></td><td><input type="submit" value="login" name="submit" id="submit" /></td></tr>
+	        	</form>
+	        	</table>
+	    	</div>
+    	</div>
+	</body>
+</html>
